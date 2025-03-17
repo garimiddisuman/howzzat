9429eb570b7dcc818b43115c680d529c92ef9bef
praneethandukuri <152866355+praneethandukuri@users.noreply.github.com>
2025-03-17 22:43:54 +0530

    add wicket keeper and bowler to field-positions

--- src/card.js
+++ src/card.js
@@ -1,73 +1,74 @@
-// implementation of cards
+import positions from "../data/field-positions.json" with {type : "json"};
+
 const cards = [
   {
-    shot: 'Cover Drive',
-    position: 'Cover',
-    fielderPresent: { wicket: { outType: 'Caught' }, runsScored: 0 },
+    shot: "Cover Drive",
+    position: "9",
+    fielderPresent: { wicket: { outType: "Caught" }, runsScored: 0 },
     fielderAbsent: { runsScored: 4 },
   },
   {
-    shot: 'Pull Shot',
-    position: 'Mid Wicket',
-    fielderPresent: { wicket: { outType: 'Caught' }, runsScored: 0 },
+    shot: "Pull Shot",
+    position: "13",
+    fielderPresent: { wicket: { outType: "Caught" }, runsScored: 0 },
     fielderAbsent: { runsScored: 6 },
   },
   {
-    shot: 'Straight Drive',
-    position: 'Mid On',
+    shot: "Straight Drive",
+    position: "12",
     fielderPresent: { runsScored: 1 },
     fielderAbsent: { runsScored: 4 },
   },
   {
-    shot: 'Cut Shot',
-    position: 'Point',
-    fielderPresent: { wicket: { outType: 'Caught' }, runsScored: 0 },
+    shot: "Cut Shot",
+    position: "5",
+    fielderPresent: { wicket: { outType: "Caught" }, runsScored: 0 },
     fielderAbsent: { runsScored: 4 },
   },
   {
-    shot: 'Lofted Shot',
-    position: 'Long Off',
-    fielderPresent: { wicket: { outType: 'Caught' }, runsScored: 0 },
+    shot: "Lofted Shot",
+    position: "21",
+    fielderPresent: { wicket: { outType: "Caught" }, runsScored: 0 },
     fielderAbsent: { runsScored: 6 },
   },
   {
-    shot: 'Glance',
-    position: 'Fine Leg',
+    shot: "Glance",
+    position: "16",
     fielderPresent: { runsScored: 1 },
     fielderAbsent: { runsScored: 4 },
   },
   {
-    shot: 'Defensive Block',
-    position: 'Silly Point',
+    shot: "Defensive Block",
+    position: "6",
     fielderPresent: { runsScored: 0 },
     fielderAbsent: { runsScored: 0 },
   },
   {
-    shot: 'Sweep Shot',
-    position: 'Square Leg',
-    fielderPresent: { wicket: { outType: 'Caught' }, runsScored: 0 },
+    shot: "Sweep Shot",
+    position: "15",
+    fielderPresent: { wicket: { outType: "Caught" }, runsScored: 0 },
     fielderAbsent: { runsScored: 4 },
   },
   {
-    shot: 'Upper Cut',
-    position: 'Third Man',
-    fielderPresent: { wicket: { outType: 'Caught' }, runsScored: 0 },
+    shot: "Upper Cut",
+    position: "23",
+    fielderPresent: { wicket: { outType: "Caught" }, runsScored: 0 },
     fielderAbsent: { runsScored: 6 },
   },
   {
-    shot: 'Helicopter Shot',
-    position: 'Mid On',
+    shot: "Helicopter Shot",
+    position: "12",
     fielderPresent: { runsScored: 2 },
     fielderAbsent: { runsScored: 6 },
   },
 ];
 
 const jsonData = JSON.stringify(cards);
-const filePath = '../data/cards.json';
+const filePath = "../data/cards.json";
 
 try {
   await Deno.writeTextFile(filePath, jsonData);
   console.log(`Data has been successfully written to ${filePath}`);
 } catch (error) {
-  console.error('Error writing to file:', error);
+  console.error("Error writing to file:", error);
 }
