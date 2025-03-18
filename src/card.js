d98a95b5bca84dcf09b3cce05c7ff08040ce913d
praneethandukuri <152866355+praneethandukuri@users.noreply.github.com>
2025-03-18 10:30:32 +0530

    refactors cards

--- src/card.js
+++ src/card.js
@@ -1,64 +1,64 @@
 // import positions from "../data/field-positions.json" with {type: "json"};
-import lodash from 'lodash';
+import lodash from "lodash";
 
 const cards = [
   {
-    shot: 'Cover Drive',
-    position: '9',
-    fielderPresent: { wicket: { outType: 'Caught' }, runsScored: 0 },
+    shot: "Cover Drive",
+    position: "9",
+    fielderPresent: { wicket: { outType: "Caught" }, runsScored: 0 },
     fielderAbsent: { runsScored: 4 },
   },
   {
-    shot: 'Pull Shot',
-    position: '13',
-    fielderPresent: { wicket: { outType: 'Caught' }, runsScored: 0 },
+    shot: "Pull Shot",
+    position: "13",
+    fielderPresent: { wicket: { outType: "Caught" }, runsScored: 0 },
     fielderAbsent: { runsScored: 6 },
   },
   {
-    shot: 'Straight Drive',
-    position: '12',
+    shot: "Straight Drive",
+    position: "12",
     fielderPresent: { runsScored: 1 },
     fielderAbsent: { runsScored: 4 },
   },
   {
-    shot: 'Cut Shot',
-    position: '5',
-    fielderPresent: { wicket: { outType: 'Caught' }, runsScored: 0 },
+    shot: "Cut Shot",
+    position: "5",
+    fielderPresent: { wicket: { outType: "Caught" }, runsScored: 0 },
     fielderAbsent: { runsScored: 4 },
   },
   {
-    shot: 'Lofted Shot',
-    position: '21',
-    fielderPresent: { wicket: { outType: 'Caught' }, runsScored: 0 },
+    shot: "Lofted Shot",
+    position: "21",
+    fielderPresent: { wicket: { outType: "Caught" }, runsScored: 0 },
     fielderAbsent: { runsScored: 6 },
   },
   {
-    shot: 'Glance',
-    position: '16',
+    shot: "Glance",
+    position: "16",
     fielderPresent: { runsScored: 1 },
     fielderAbsent: { runsScored: 4 },
   },
   {
-    shot: 'Defensive Block',
-    position: '6',
+    shot: "Defensive Block",
+    position: "6",
     fielderPresent: { runsScored: 0 },
     fielderAbsent: { runsScored: 0 },
   },
   {
-    shot: 'Sweep Shot',
-    position: '15',
-    fielderPresent: { wicket: { outType: 'Caught' }, runsScored: 0 },
+    shot: "Sweep Shot",
+    position: "15",
+    fielderPresent: { wicket: { outType: "Caught" }, runsScored: 0 },
     fielderAbsent: { runsScored: 4 },
   },
   {
-    shot: 'Upper Cut',
-    position: '23',
-    fielderPresent: { wicket: { outType: 'Caught' }, runsScored: 0 },
+    shot: "Upper Cut",
+    position: "23",
+    fielderPresent: { wicket: { outType: "Caught" }, runsScored: 0 },
     fielderAbsent: { runsScored: 6 },
   },
   {
-    shot: 'Helicopter Shot',
-    position: '12',
+    shot: "Helicopter Shot",
+    position: "12",
     fielderPresent: { runsScored: 2 },
     fielderAbsent: { runsScored: 6 },
   },
@@ -95,20 +95,20 @@ class Deck {
 
 const main = () => {
   const cardDeck = new Deck(cards);
-  console.log('red Deck:', cardDeck.cards);
+  console.log("red Deck:", cardDeck.cards);
 
-  console.log('First Card Drawn:', cardDeck.drawCard());
-  console.log('Second Card Drawn:', cardDeck.drawCard());
+  console.log("First Card Drawn:", cardDeck.drawCard());
+  console.log("Second Card Drawn:", cardDeck.drawCard());
 
   if (!cardDeck.hasCards()) {
     cardDeck.reShuffle();
-    console.log('Deck Reshuffled:', cardDeck.cards);
+    console.log("Deck Reshuffled:", cardDeck.cards);
   }
 };
 
 main();
 
-export { Deck};
+export { Deck };
 // class CardDeck {
 //   constructor(cards) {
 //     this.cards = [...cards];
